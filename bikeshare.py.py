6b62cb4d915d762b11c07e80d961a081d7dc6785
Cara Alexander <cara@a4inc.ca>
2019-03-13 11:59:56 -0700

    docs: add comment to section printing what the user has selected

--- bikeshare.py.py
+++ bikeshare.py.py
@@ -74,6 +74,7 @@ def get_filters():
         else:
             print('Please enter a valid day or all.')
 
+    # tell user which city, month, and day they have selected
     print("Great! Let's look at data from " + city)
 
     if month == 'all':
